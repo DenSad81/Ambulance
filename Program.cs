7ccadd49c9cd2c5f633bf871d1255624b6fa4861
DenSad81 <tagil81@mail.ru>
2024-04-14 13:47:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        int quantityPeople;
+        int timePerHumanMinutes = 10;
+        int minutesInHour = 60;
+        int waitTotalTimeMinutes;
+        int waitHours;
+        int waitMinutes;
+
+        Console.Write("Сколько пациентов перед вами: ");
+        quantityPeople = Convert.ToInt32(Console.ReadLine());
+        waitTotalTimeMinutes = quantityPeople * timePerHumanMinutes;
+        waitHours = waitTotalTimeMinutes / minutesInHour;
+        waitMinutes = waitTotalTimeMinutes - waitHours * minutesInHour;
+        Console.WriteLine($"Вам стоять в очереди часов {waitHours}, минут {waitMinutes}");
+    }
+}
